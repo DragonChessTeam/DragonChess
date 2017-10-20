0f1b1453fa63835e58a48da29f638c34fd610da2
Bolodya <Vovan1_2@mail.ru>
2017-10-20 18:11:38 +0700

    initial commit

---
+++ src/main/java/ru/nsu/fit/g14203/Main.java
@@ -0,0 +1,8 @@
+package ru.nsu.fit.g14203;
+
+public class Main {
+
+    public static void main(String[] args) {
+        throw new RuntimeException("Empty project");    //  FIXME: start from here
+    }
+}
