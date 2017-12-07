3b05c66b7a939125a814c8c7bf509554c5d74689
Alexandr Shelekhin <ultrashe@gmail.com>
2017-12-07 20:50:01 +0700

    Init commit for 13-Shelekhin branch
    Signed-off-by: Alexandr Shelekhin <ultrashe@gmail.com>

--- src/main/java/ru/nsu/fit/g14203/Main.java
+++ src/main/java/ru/nsu/fit/g14203/Main.java
@@ -3,7 +3,7 @@ package ru.nsu.fit.g14203;
 public class Main {
 
     public static void main(String[] args) {
-        //NOW IN DEV BRANCH!
+        //NOW IN 13-Shelekhin BRANCH!
         throw new RuntimeException("Empty project");    //  FIXME: start from here
     }
 }
