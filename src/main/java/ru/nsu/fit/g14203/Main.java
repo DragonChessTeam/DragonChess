d4392eb6c7270260cd41cb271419991bfbc53046
Alexandr Shelekhin <ultrashe@gmail.com>
2017-12-07 20:47:44 +0700

    Init commit for dev branch
    Signed-off-by: Alexandr Shelekhin <ultrashe@gmail.com>

--- src/main/java/ru/nsu/fit/g14203/Main.java
+++ src/main/java/ru/nsu/fit/g14203/Main.java
@@ -3,6 +3,7 @@ package ru.nsu.fit.g14203;
 public class Main {
 
     public static void main(String[] args) {
+        //NOW IN DEV BRANCH!
         throw new RuntimeException("Empty project");    //  FIXME: start from here
     }
 }
