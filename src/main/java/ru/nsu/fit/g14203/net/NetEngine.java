6ef14fc95bd53d259b13e44e1f89dd0a5e5a4770
Bolodya <Vovan1_2@mail.ru>
2017-12-20 21:39:47 +0700

    doc(network): add documentation comments for NetEngine

--- src/main/java/ru/nsu/fit/g14203/net/NetEngine.java
+++ src/main/java/ru/nsu/fit/g14203/net/NetEngine.java
@@ -5,85 +5,48 @@ import ru.nsu.fit.g14203.engine.api.Observer;
 import ru.nsu.fit.g14203.engine.api.utils.Color;
 import ru.nsu.fit.g14203.engine.api.utils.EngineResponse;
 import ru.nsu.fit.g14203.engine.api.utils.Way;
-import ru.nsu.fit.g14203.net.util.*;
 
 import java.io.IOException;
 import java.net.SocketAddress;
 import java.util.function.Consumer;
 
-public class NetEngine implements Engine, MessageReceiver {
+public class NetEngine implements Engine {
 
     public enum Type {
         SERVER,
         CLIENT
     }
 
-    private final Engine engine;
-    private final GameThread thread;
-
+    /**
+     * Creates Engine for network game.
+     *
+     * @param type can be SERVER for start game or CLIENT for join game
+     * @param address local address for SERVER of remote address for CLIENT
+     * @param engine engine to use in game
+     * @param connectHandler method to be called after accept (SERVER) or connect (CLIENT)
+     * @param disconnectHandler method to be called after opponent disconnect
+     * @throws IOException if some IO errors with sockets occurs during initialisation
+     */
     public NetEngine(Type type, SocketAddress address, Engine engine,
                      Consumer<Color> connectHandler, Runnable disconnectHandler)
             throws IOException {
-        this.engine = engine;
-
-        final Consumer<Color> __connect = color -> {
-            subscribe();
-            connectHandler.accept(color);
-        };
-
-        if (type == Type.SERVER)
-            thread = new ServerThread(address, __connect, disconnectHandler);
-        else
-            thread = new ClientThread(address, __connect, disconnectHandler);
-
-        thread.start();
-    }
-
-    private void subscribe() {
-        thread.getMessageChannel().addReceiver(this);
     }
 
     @Override
     public EngineResponse doCapture(Color color, Way turn) {
-        final EngineResponse response = engine.doCapture(color, turn);
-
-        thread.getMessageChannel().send(new StepMessage(Message.TYPE_CAPTURE, color, turn));
-
-        return response;
+        return null;
     }
 
     @Override
     public EngineResponse doMove(Color color, Way turn) {
-        final EngineResponse response = engine.doMove(color, turn);
-
-        thread.getMessageChannel().send(new StepMessage(Message.TYPE_MOVE, color, turn));
-
-        return response;
+        return null;
     }
 
     @Override
     public void registerObserver(Observer observer) {
-        engine.registerObserver(observer);
     }
 
     @Override
     public void deleteObserver(Observer observer) {
-        engine.deleteObserver(observer);
-    }
-
-    @Override
-    public void receive(Message message) {
-        if (message instanceof DisconnectMessage) {
-            thread.interrupt();
-            return;
-        }
-
-        if (message instanceof StepMessage) {
-            final StepMessage step = (StepMessage) message;
-            if (step.getType() == Message.TYPE_CAPTURE)
-                doCapture(step.getColor(), step.getWay());
-            else
-                doMove(step.getColor(), step.getWay());
-        }
     }
 }
